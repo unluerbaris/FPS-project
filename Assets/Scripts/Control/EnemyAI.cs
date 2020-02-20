d8b534281513a55a337478e147b7619a06c5da1f
unluerbaris <unluerbaris@gmail.com>
2020-02-20 15:10:23 +0900

    Enemy follows the player

---
+++ Assets/Scripts/Control/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ES.Control
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        NavMeshAgent navMeshAgent;
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
