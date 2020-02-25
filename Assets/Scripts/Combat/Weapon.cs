2e7fe6ec6f578b52b3751b83f824bf212b31d5db
Barış Ünlüer <51197621+unluerbaris@users.noreply.github.com>
2020-02-25 14:59:52 +0900

    Raycast from the camera

---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ES.Combat
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] Camera FPSCamera;
+        [SerializeField] float range = 100f;
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            RaycastHit hit;
+            Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range);
+            Debug.Log("Hits " + hit.transform.name);
+        }
+    }
+}
