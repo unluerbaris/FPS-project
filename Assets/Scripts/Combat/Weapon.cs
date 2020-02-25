4f2c4328c5ebd5749df829b676c74587f20df5a5
Barış Ünlüer <51197621+unluerbaris@users.noreply.github.com>
2020-02-25 15:41:43 +0900

    Enemy Health  script and TakeDamage method

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ES.Core;
 
 namespace ES.Combat
 {
@@ -8,6 +9,7 @@ namespace ES.Combat
     {
         [SerializeField] Camera FPSCamera;
         [SerializeField] float range = 100f;
+        [SerializeField] float damage = 20f;
 
         void Update()
         {
@@ -20,8 +22,20 @@ namespace ES.Combat
         private void Shoot()
         {
             RaycastHit hit;
-            Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range);
-            Debug.Log("Hits " + hit.transform.name);
+            bool hasHit = Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range);
+            if (hasHit)
+            {
+                Debug.Log("Hits " + hit.transform.name);
+
+                EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+                if (target == null) return;
+
+                target.TakeDamage(damage);
+            }
+            else // if raycast can't hit anything, just return
+            {
+                return;
+            }
         }
     }
 }
