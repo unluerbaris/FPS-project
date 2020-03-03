a1a44433b528d4b50919d2e8ee115304b93f12f7
Barış Ünlüer <51197621+unluerbaris@users.noreply.github.com>
2020-03-03 10:05:24 +0900

    Enemy attack event

---
+++ Assets/Scripts/Combat/EnemyAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ES.Combat
+{
+    public class EnemyAttack : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        [SerializeField] float damage = 10f;
+
+        void Start()
+        {
+
+        }
+
+        public void AttackHitEvent() // Animation event
+        {
+            if (target == null) return;
+            Debug.Log("Enemy is attacking to " + target.name);
+        }
+    }
+}
