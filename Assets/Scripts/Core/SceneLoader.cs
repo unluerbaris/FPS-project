8bd66711ab5696845ddf5790ce9224dc03e9dafa
Barış Ünlüer <51197621+unluerbaris@users.noreply.github.com>
2020-03-03 12:30:46 +0900

    Created a game over canvas

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ES.Core
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadCurrentLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
