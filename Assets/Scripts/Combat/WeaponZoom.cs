5cbdf63b027ae3c5210fb0a0caf73643d2989481
Barış Ünlüer <51197621+unluerbaris@users.noreply.github.com>
2020-03-04 22:46:00 +0900

    Created weapon zoom function

---
+++ Assets/Scripts/Combat/WeaponZoom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace ES.Combat
+{
+    public class WeaponZoom : MonoBehaviour
+    {
+        [SerializeField] Camera fpsCamera;
+        [SerializeField] float zoomInFovValue = 25f;
+        [SerializeField] float zoomOutFovValue = 60f;
+        [SerializeField] float zoomInMouseSensitivity = 2f;
+        [SerializeField] float zoomOutMouseSensitivity = 4.5f;
+
+        bool zoomedIn = false;
+
+        RigidbodyFirstPersonController firstPersonController;
+
+        private void Start()
+        {
+            firstPersonController = GetComponent<RigidbodyFirstPersonController>();
+
+            // Set the mouse sensitivity at start
+            firstPersonController.mouseLook.XSensitivity = zoomOutMouseSensitivity;
+            firstPersonController.mouseLook.YSensitivity = zoomOutMouseSensitivity;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1)) // Right mouse button value
+            {
+                ChangeZoomingValues();
+            }
+        }
+
+        private void ChangeZoomingValues()
+        {
+            if (zoomedIn)
+            {
+                ZoomOut();
+                return;
+            }
+
+            ZoomIn();
+        }
+
+        private void ZoomIn()
+        {
+            zoomedIn = true;
+            fpsCamera.fieldOfView = zoomInFovValue; // to zoom in change the Field of View value
+
+            // Change the mouse sensitivity values
+            firstPersonController.mouseLook.XSensitivity = zoomInMouseSensitivity;
+            firstPersonController.mouseLook.YSensitivity = zoomInMouseSensitivity;
+        }
+
+        private void ZoomOut()
+        {
+            zoomedIn = false;
+            fpsCamera.fieldOfView = zoomOutFovValue;
+
+            // Change the mouse sensitivity values
+            firstPersonController.mouseLook.XSensitivity = zoomOutMouseSensitivity;
+            firstPersonController.mouseLook.YSensitivity = zoomOutMouseSensitivity;
+        }
+    }
+}
