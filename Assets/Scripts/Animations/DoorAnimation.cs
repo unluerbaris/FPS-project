c882db413f451e34aa087d8273243cfa80a85f06
Baris Unluer <unluerbaris@gmail.com>
2020-04-12 17:22:54 +0900

    Added door opening SFX

--- Assets/Scripts/Animations/DoorAnimation.cs
+++ Assets/Scripts/Animations/DoorAnimation.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ES.Control;
+using ES.Audio;
 
 namespace ES.Animations
 {
     public class DoorAnimation : MonoBehaviour
     {
+        [SerializeField] AudioCollections doorOpenSFX;
         float distanceFromPlayer;
+        bool isOpen = false;
         GameObject target;
 
         private void Update()
@@ -15,10 +18,23 @@ namespace ES.Animations
             distanceFromPlayer = FPSController.distanceFromTarget;
             target = FPSController.targetObject;
 
-            if (this.gameObject == target && Input.GetKeyDown(KeyCode.E) && distanceFromPlayer < 2f)
+            if (this.gameObject == target && Input.GetKeyDown(KeyCode.E) && distanceFromPlayer < 2f && !isOpen)
             {
+                isOpen = true;
                 Animation doorAnimation = target.GetComponent<Animation>();
+                PlaySFX(doorOpenSFX);
                 doorAnimation.Play();
+            }
+        }
+
+        public void PlaySFX(AudioCollections soundFX)
+        {
+            if (AudioManager.instance != null && soundFX != null)
+            {
+                AudioClip soundToPlay;
+                soundToPlay = soundFX[0];
+                AudioManager.instance.PlayOneShotSound("Scene", soundToPlay, transform.position,
+                                                      soundFX.volume, soundFX.spatialBlend, soundFX.priority);
             }
         }
     }
