3881d32a7b20d1bb2c03c1f205d137ce8dccc1dd
Baris Unluer <unluerbaris@gmail.com>
2020-07-23 22:52:09 +0900

    set target fps

---
+++ Assets/Scripts/Core/TargetFPS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ES.Core
+{
+    public class TargetFPS : MonoBehaviour
+    {
+        void Awake()
+        {
+            Application.targetFrameRate = 30;
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
